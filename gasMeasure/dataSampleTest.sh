c19c46d0b889f8865cd2c6b78998891fb4fd0f60
e3jake <e3jake@gmail.com>
2021-08-30 16:16:52 +0900

    Update dataSampleTest.sh

--- gasMeasure/dataSampleTest.sh
+++ gasMeasure/dataSampleTest.sh
@@ -16,7 +16,7 @@ echo "front $random_f rear $random_r Press [CTRL+C] to stop.."
 #gasdb,host=drone rear_m=731'
 ####
 
-curl -X POST 'http://127.0.0.1:8086/write?db=gasdb&u=gasadmin&p=gasadmin' --data-binary "gasdb,host=drone default_m=$d_m
+curl -X POST 'http://127.0.0.1:8086/write?db=gasdb&u=gasadmin&p=9AvamVRQ5Wfi9vKvpzynZT1WDXrCJL' --data-binary "gasdb,host=drone default_m=$d_m
 gasdb,host=drone front_m=$random_f
 gasdb,host=drone rear_m=$random_r"
 
